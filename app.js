b188b5292b2316418574fe948437313cf4627dca
tuankhai1998 <honganh7434@gmail.com>
2019-06-02 22:19:13 +0700

    update on 2/6 in night

--- app.js
+++ app.js
@@ -250,6 +250,21 @@ function removeItemCart() {
     // body
 }
 
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
 window.addEventListener('load', function (e) {
 
     addItemToFavorite();
@@ -263,8 +278,7 @@ window.addEventListener('load', function (e) {
     showMenu();
 
     removeItemCart();
-
-
+    
 });
 
 
